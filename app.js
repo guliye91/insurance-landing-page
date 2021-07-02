4a5caa406323c6e77a9fc06659187801a32d4298
captain guliye <guliye91@gmail.com>
2021-07-02 10:41:40 +0300

    added close navigation component

--- app.js
+++ app.js
@@ -1,14 +1,25 @@
 const hamburger = document.querySelector('.hamburger-icon');
+const closeIcon = document.querySelector('.close-icon')
 const menu = document.querySelector('.main-menu');
 const links = document.querySelectorAll('.main-menu a');
 
 
 hamburger.addEventListener('click', ()=>{
-    menu.classList.toggle('show');
+    menu.classList.add('show');
+    hamburger.style.display = 'none';
+    closeIcon.style.cssText = 'display:block;';
+
+})
+closeIcon.addEventListener('click',()=>{
+    menu.classList.remove('show');
+    closeIcon.style.display = 'none';
+    hamburger.style.display = "block";
 })
 
 links.forEach((link)=>{
     link.addEventListener('click',()=>{
         menu.classList.remove('show');
+        closeIcon.style.display = 'none';
+    hamburger.style.display = "block";
     })
 })
